3e4b9a0963cae55f98940cc5fbc2f2cbe9dbda8b
AertsMatthew <matthew.aerts@telenet.be>
2025-08-09 23:57:00 +0200

    adjust placeholder text in login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -218,7 +218,7 @@ button:hover {
 
 .input-group {
   position: relative;
-  overflow: visible; /* voorkomt dat iconen worden geknipt */
+  overflow: visible; /* voorkomt dat iconen worden 
 }
 
 .input-group input {
